d81d1e730b313e1dcc0450ea3f0caf407d693119
Huzaifa Al Mesbah <hi@huzaifa.im>
2025-02-08 23:06:27 +0600

    Remove white space

--- templates/contributors-list.php
+++ templates/contributors-list.php
@@ -37,12 +37,7 @@ $version                 = $version ?? '';
 		<div class="wpcg-header-controls">
 			<?php if ( $version_switcher ) : ?>
 				<div class="wpcg-version-dropdown">
-					<input type="text" class="wpcg-version-input" readonly value="
-					<?php
-					/* translators: %s: WordPress version number */
-					printf( esc_html__( 'WordPress %s', 'contributors-gallery' ), esc_html( $version ) );
-					?>
-					">
+					<input type="text" class="wpcg-version-input" readonly value="<?php printf( esc_html__( 'WordPress %s', 'contributors-gallery' ), esc_html( $version ) ); ?>">
 					<ul class="wpcg-version-list">
 						<?php
 						// Sort versions in descending order
