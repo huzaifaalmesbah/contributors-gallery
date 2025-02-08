39da12be30a115f10e320319929f91eef8a630ee
Huzaifa Al Mesbah <hi@huzaifa.im>
2025-02-08 21:45:23 +0600

    fix: improve HTML output formatting in noteworthy contributors template
    - Remove unnecessary whitespace and line breaks in HTML output
    - Inline PHP echo statements with HTML attributes
    - Maintain code readability while ensuring clean rendered output

--- templates/partials/noteworthy-contributors.php
+++ templates/partials/noteworthy-contributors.php
@@ -28,40 +28,13 @@ if ( empty( $contributors ) ) {
 	
 	<div class="wpcg-contributor-list">
 		<?php foreach ( $contributors as $username => $user_data ) : ?>
-			<a href="
-			<?php
-			echo esc_url(
-				sprintf(
-					'https://profiles.wordpress.org/%s',
-					$username
-				)
-			);
-			?>
-			" 
-			target="_blank" 
-			rel="noopener noreferrer"
-			class="wpcg-contributor-item">
+			<a href="<?php echo esc_url( sprintf( 'https://profiles.wordpress.org/%s', $username ) ); ?>" target="_blank" rel="noopener noreferrer" class="wpcg-contributor-item">
 				<?php if ( ! empty( $user_data[1] ) ) : ?>
-					<img src="
-					<?php
-					echo esc_url(
-						sprintf(
-							'https://secure.gravatar.com/avatar/%s?s=80&d=mm&r=g',
-							$user_data[1]
-						)
-					);
-					?>
-					" 
-					alt="<?php echo esc_attr( $user_data[0] ); ?>"
-					class="wpcg-avatar">
+					<img src="<?php echo esc_url( sprintf( 'https://secure.gravatar.com/avatar/%s?s=200&d=mm&r=g', $user_data[1] ) ); ?>" alt="<?php echo esc_attr( $user_data[0] ); ?>" class="wpcg-avatar">
 				<?php endif; ?>
-
 				<h4><?php echo esc_html( $user_data[0] ); ?></h4>
-
 				<?php if ( ! empty( $user_data[3] ) ) : ?>
-					<span class="wpcg-role">
-						<?php echo esc_html( $user_data[3] ); ?>
-					</span>
+					<span class="wpcg-role"><?php echo esc_html( $user_data[3] ); ?></span>
 				<?php endif; ?>
 			</a>
 		<?php endforeach; ?>
